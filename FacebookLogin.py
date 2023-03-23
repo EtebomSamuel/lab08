15a644cfeebee2ab1207e1b05382eebda15cde8b
EtebomSamuel <128656985+EtebomSamuel@users.noreply.github.com>
2023-03-22 23:03:05 -0400

    Create FacebookLogin.py

---
+++ FacebookLogin.py
@@ -0,0 +1,11 @@
+userName = input ("Hello ! Please enter your facebook name!")
+
+userPassword = input ("Hello ! Please enter your facebook password!")
+
+if(userName.lower() == "etebom" and userPassword == "EmergenC"):
+    print("Welcome to Facebook!")
+else:
+    print("Wrong Credentials,try again!")
+
+
+#print ("Hello", userName, "nice to meet you")
